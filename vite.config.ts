555bf909da50b4829e0622e552e399ddecdbc476
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-20 14:31:12 +0100

    feat!: Update to Nuxt UI 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import uiPro from '@nuxt/ui-pro/vite'
+import ui from '@nuxt/ui/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue(), uiPro({
+  plugins: [vue(), ui({
     ui: {
       colors: {
         primary: 'rose',
